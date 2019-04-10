89db080319ad7c0ea200298b9e44b369f131d356
Anton Smarovydlo <anton.smarovydlo@spryker.com>
2019-04-10 18:51:12 +0300

    PS-9070: amendments after TL review

--- src/Spryker/Client/QuoteRequestExtension/Dependency/Plugin/QuoteRequestQuoteCheckPluginInterface.php
+++ src/Spryker/Client/QuoteRequestExtension/Dependency/Plugin/QuoteRequestQuoteCheckPluginInterface.php
@@ -9,7 +9,7 @@ namespace Spryker\Client\QuoteRequestExtension\Dependency\Plugin;
 
 use Generated\Shared\Transfer\QuoteTransfer;
 
-interface QuoteRequestCreatePreCheckPluginInterface
+interface QuoteRequestQuoteCheckPluginInterface
 {
     /**
      * Specification:
